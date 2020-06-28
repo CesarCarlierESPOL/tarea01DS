a1b8435c316c59fdaf928ce9f8d2971010cbbd13
Karla Castro Ascencio <kycastro@espol.edu.ec>
2020-06-27 21:40:22 -0500

    Agregar jars de javafx

---
+++ observer/src/vistas/Principal.java
@@ -0,0 +1,59 @@
+package vistas;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+import javafx.scene.control.Button;
+
+
+public class Principal extends Application {
+	
+	private final AnchorPane root = new AnchorPane();
+	private Scene scene;
+	
+	private final Button redButton = new Button("Rojo");
+	private final Button blueButton = new Button("Azul");
+	private final Button yellowButton = new Button("Amarillo");
+	private final HBox hboxButtons = new HBox();
+	
+	private void configureButtonStyles() {
+		hboxButtons.setSpacing(180);
+		hboxButtons.getChildren().addAll(redButton, yellowButton, blueButton);
+		
+		AnchorPane.setBottomAnchor(hboxButtons, 20.0);
+		AnchorPane.setLeftAnchor(hboxButtons, 60.0);
+	}
+	
+	private void configureActions() {
+		redButton.setOnAction(e -> root.setStyle("-fx-background-color: red;"));
+		blueButton.setOnAction(e -> root.setStyle("-fx-background-color: blue;"));
+		yellowButton.setOnAction(e -> root.setStyle("-fx-background-color: yellow;"));
+	}
+	
+	@Override
+	public void start(Stage stage) {
+		configureButtonStyles();
+		configureActions();
+		
+		root.getChildren().add(hboxButtons);
+		
+		scene = new Scene(root, 650, 400);
+		stage.setScene(scene);
+		stage.setTitle("Observer");
+		stage.setResizable(false);
+		stage.show();
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Cerrando aplicacion...");
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
